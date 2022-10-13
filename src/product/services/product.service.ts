b092c5a0bc7a62be6eb7ae44e0db3d78572d246c
Victor França <vito.daleste@gmail.com>
2022-10-13 16:42:37 -0300

    Metodo Update e Delete

--- src/product/services/product.service.ts
+++ src/product/services/product.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { ILike, Repository } from "typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 import { Product } from "../entities/product.entity";
 
 
@@ -40,5 +40,23 @@ export class ProductService{
     async create(product: Product): Promise<Product>{
         return await this.productRepository.save(product);
     }
-}
 
+    async update(product: Product): Promise<Product> {
+        let searchProduct: Product = await this.findById(product.id);
+
+        if (!searchProduct || !product.id)
+            throw new HttpException('Produto não encontrado', HttpStatus.NOT_FOUND);
+        
+        return await this.productRepository.save(product);
+        
+    }
+
+    async delete(id: number): Promise<DeleteResult>{
+        let searchProduct = await this.findById(id);
+
+        if (!searchProduct)
+            throw new HttpException('Produto não encontrado', HttpStatus.NOT_FOUND);
+        return await this.productRepository.delete(id);
+
+    }
+}
