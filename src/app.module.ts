7df2d1bbc829e9d1bf86f214d81881cba1677a78
julio cesar <juliocgcj@hotmail.com>
2022-10-13 10:45:36 -0300

    Base nestJS e conexao BD

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot ({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_womenwork',
+      entities: [],
+      synchronize: true
+    })
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
