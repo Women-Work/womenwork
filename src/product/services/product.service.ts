b538e57cbc8a74388721a7fee3d86d5b8414dcb5
Victor França <vito.daleste@gmail.com>
2022-11-01 15:31:45 -0300

    update: product class documentation

--- src/product/services/product.service.ts
+++ src/product/services/product.service.ts
@@ -10,6 +10,10 @@ export class ProductService {
     private productRepository: Repository<Product>,
   ) {}
 
+  /**
+   * @desc Search all products registered in the database
+   * @return A promise to search for everything
+   */
   async findAll(): Promise<Product[]> {
     return await this.productRepository.find({
       relations:{
@@ -19,6 +23,13 @@ export class ProductService {
     });
   }
 
+
+  /**
+   * @desc Search product by id registered in the database
+   * @returns A promise to search for a product id 
+   * @Param id that will be searched in the database
+   * @throw HttpException in case the id is not found in the database.
+   */
   async findById(id: number): Promise<Product> {
     const product = await this.productRepository.findOne({
       where: {
@@ -36,6 +47,11 @@ export class ProductService {
     return product;
   }
 
+ /**
+   * @desc Search product by name registered in the database
+   * @returns A promise to search for a product title
+   * @Param name that will be searched in the database
+   */
   async findByTitle(title: string): Promise<Product[]> {
     return await this.productRepository.find({
       where: { title: ILike(`%${title}%`) 
@@ -47,10 +63,20 @@ export class ProductService {
     });
   }
 
+  /**
+   * @desc add new product to database
+   * @Body where the order should be placed
+   * @returns a promise that the product was registered in the database
+   */
   async create(product: Product): Promise<Product> {
     return await this.productRepository.save(product);
   }
 
+  /**
+   * @desc updates a product that is registered in the database through the ID
+   * @Body where the order should be placed
+   * @returns a promise that the product has been updated in the database
+   */
   async update(product: Product): Promise<Product> {
     const searchProduct: Product = await this.findById(product.id);
 
@@ -60,6 +86,11 @@ export class ProductService {
     return await this.productRepository.save(product);
   }
 
+  /**
+   * @desc remove a product from the database
+   * @param id parameter to remove the user from the database
+   * @returns a promise that the user has been removed from the database
+   */
   async delete(id: number): Promise<DeleteResult> {
     const searchProduct = await this.findById(id);
 
