576ef3a68ca545695ce71a5fb246f24aab23f401
xHenrique22 <henrique.oliveira9823@gmail.com>
2022-11-30 13:03:44 -0300

    feat: form added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Form from './components/Form/Form';
+
 
 function App() {
   return (
-    
+    <>
+      <Form/>
+      
+    </>
   );
 }
 
