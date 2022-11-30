e51877d4562b887d91887aaa490ae6bf738bee8c
raffaez <rafaeleasouzas@gmail.com>
2022-11-30 10:50:38 -0300

    feat: theme

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#766B8D',
+    },
+    secondary: {
+      main: '#E1A6A0',
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
