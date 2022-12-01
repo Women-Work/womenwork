1188ea3b2c82a399fa80c5c6abfd8aef61ac43fd
raffaez <rafaeleasouzas@gmail.com>
2022-12-01 01:18:37 -0300

    update(courses): style cards

--- src/pages/courses/Courses.tsx
+++ src/pages/courses/Courses.tsx
@@ -2,20 +2,47 @@ import { Grid } from '@material-ui/core';
 import React from 'react';
 import Card from '../../components/card/Card';
 
+const courses = [
+  {
+    title: 'JavaScript',
+    text: 'Curso de JavaScript',
+    img: 'js',
+    price: 20
+  },
+  {
+    title: 'HTML e CSS',
+    text: 'Curso de HTML e CSS',
+    img: 'htmlcss',
+    price: 20,
+  },
+  {
+    title: 'MYSQL',
+    text: 'Curso de MYSQL',
+    img: 'mysql',
+    price: 20,
+  }
+]
+
 function Courses() {
   return (
     <>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6} lg={3}>
-          <Card title="Título" text="laalalalaala" price={0} />
+    <Grid
+      container
+      spacing={5}
+      alignItems="center"
+      justifyContent="center"
+      style={{ padding: 20 }}
+    >
+      {courses.map((course) => (
+        <Grid item xs={10} md={5} lg={3}>
+          <Card
+            title={course.title}
+            text={course.text}
+            img={course.img}
+            price={course.price} />
         </Grid>
-        <Grid item xs={12} md={6} lg={3}>
-          <Card title="Título" text="laalalalaala" price={0} />
-        </Grid>
-        <Grid item xs={12} md={6} lg={3}>
-          <Card title="Título" text="laalalalaala" price={0} />
-        </Grid>
-      </Grid>
+      ))}
+    </Grid>
     </>
   )
 }
