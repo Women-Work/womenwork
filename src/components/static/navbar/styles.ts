03465aabf870c019566e37af7ee2f0bd7d90ede3
João Vitor Estevam <joaophvt@gmail.com>
2022-12-01 16:01:01 -0300

    feat: style created for Navbar

---
+++ src/components/static/navbar/styles.ts
@@ -0,0 +1,62 @@
+import { alpha, createStyles, makeStyles, Theme } from "@material-ui/core";
+
+const styles = makeStyles((theme: Theme) => createStyles({
+  navbar: {
+    backgroundColor: theme.palette.primary.main,
+    marginBottom: '1rem',
+    position: 'absolute'
+  },
+  cartIcon: {
+    cursor: 'pointer',
+    color: theme.palette.primary.contrastText
+  },
+  buttons: {
+    color: theme.palette.primary.contrastText,
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    }
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  }
+}));
+
+export { styles };
