566c2b166c67b1a8c5bc69c6a5df9a50a64bab90
Victor França <vito.daleste@gmail.com>
2022-12-01 11:02:49 -0300

    feat: footer created

--- src/components/static/footer/Footer.tsx
+++ src/components/static/footer/Footer.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import { Box } from '@mui/material';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import React from "react";
+import "./Footer.css"
+import { Grid, Typography } from '@material-ui/core';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
-const Footer = () => {
+function Footer() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Grid className='footer' container>
+        <Grid xs={12}>
+            <Box display="flex" alignItems="center" justifyContent="center" className='footer-color'>
+              <Typography variant="h6" align="center" gutterBottom className='text-color'>Women Work</Typography>
+          </Box>
+          <Box className='footer-copy' display="flex" justifyContent="space-around">
+            <Box display='flex' justifyContent='center' alignItems='center'>
+              <a href="#" target="_blank">
+                <FacebookIcon className='social' />
+              </a>
+              <a href="https://github.com/Women-Work/womenwork" target="_blank">
+                <GitHubIcon className='social' />
+              </a>
+              <a href="#" target="_blank">
+                <LinkedInIcon className='social' />
+              </a>
+            </Box>
+            <Box>
+              <Typography variant="subtitle1" align="center" gutterBottom className='text-color'>© 2020 Copyright</Typography>
+            </Box>
+            <Box style={{ display: 'flex', justifyContent: 'right' }}>
+              <Typography variant="subtitle1" align="center" gutterBottom className='text-color'> Sobre nós </Typography>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
   )
 }
 
-export default Footer
+export default Footer ;
