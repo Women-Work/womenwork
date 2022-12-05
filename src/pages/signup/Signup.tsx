a45090946654969fbc0b8e6050a561a253aa6bce
xHenrique22 <henrique.oliveira9823@gmail.com>
2022-12-05 08:33:22 -0300

    minor fixes

--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -58,13 +58,13 @@ export default function Singup() {
 
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid item xs={3} sm={6} className='imagem2'></Grid>
-            <Grid item xs={3} alignItems='center'>
+            <Grid item xs={3} sm={4} alignItems='center'>
                 <Box paddingX={10}>
                     <form onSubmit={onSubmit}>
                         <Typography variant='h2' gutterBottom color='textPrimary' component='h3' align='center' className='textos2'>Cadastre-se</Typography>
                         <TextField value={user.name} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='nome' label='Nome' variant='outlined' name='name' margin='normal' required fullWidth />
                         <TextField value={user.user} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='Usuario' variant='outlined' name='user' margin='normal' required fullWidth type='email'/>
-                        <TextField value={user.password} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='password' margin='normal' type='password' required fullWidth />
+                        <TextField value={user.password} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='Senha' variant='outlined' name='password' margin='normal' type='password' required fullWidth />
                         <TextField value={confirmPassword} onChange={(e:ChangeEvent<HTMLInputElement>) => confirmPasswordHandle(e)} id='confirmarSenha' label='Confirmar senha' variant='outlined' name='confirmarSenha' margin='normal' type='password' required fullWidth />
                         <Box marginTop={2} textAlign='center'>
                             <Link to='/login' className='text-decorator-none'>
