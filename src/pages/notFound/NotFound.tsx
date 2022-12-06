30dbf4b5f32014141ea7a1f01eb8a09ee631d490
raffaez <rafaeleasouzas@gmail.com>
2022-12-06 08:20:21 -0300

    fix: center not found page

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -5,12 +5,12 @@ import './NotFound.css'
 function NotFound() {
   return (
     <>
-      <Grid container direction='row' alignItems='center'>
-        <Grid item xs={6} sm={12} className='grid-1'>
-          <img className='imgNot' src="assets/images/WomenWork_not.svg" alt="" />
-        </Grid>
-        <Grid item xs={6} sm={12}>
+      <Grid container direction='column' alignItems='center' justifyContent='center'>
+        <Grid item xs={12}>
           <h1 className='not'>404: Página não encontrada</h1>
+        </Grid>
+        <Grid item xs={12} className='grid-1'>
+          <img className='imgNot' src="assets/images/WomenWork_not.svg" alt="" />
         </Grid>
       </Grid>
     </>
