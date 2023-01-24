cad5e9fb556a16b027b9a7d54d920232b078cc00
raffaez <rafaeleasouzas@gmail.com>
2023-01-23 21:18:27 -0300

    refactor: implement custom Grid component

--- src/components/courses/listCourses/ListCourses.tsx
+++ src/components/courses/listCourses/ListCourses.tsx
@@ -1,13 +1,14 @@
 import './ListCourses.css';
 
-import { Grid, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 
 import { useAppDispatch, useAppSelector } from '../../../common/hooks';
 import { fetchCourses, getCoursesError, getCoursesStatus, selectAllCourses } from '../../../store/coursesSlice';
+import { selectToken } from '../../../store/tokenSlice';
 import Card from '../../cardCourse/CardCourse';
 import Loading from '../../static/loading/Loading';
-import { selectToken } from '../../../store/tokenSlice';
+import Grid from '../../../common/Grid';
 
 function Courses() {
   const token = useAppSelector(selectToken);
@@ -27,33 +28,30 @@ function Courses() {
   if(status === 'loading') {
     content = <Loading />
   } else if(status === 'succeeded') {
-    content = courses.map((course) => (
-      <Card
-        key={course.id}
-        id={course.id}
-        title={course.title}
-        description={course.description}
-        price={course.price}
-      />
-    ))
+    if(courses.length > 0) {
+      content = courses.map((course) => (
+        <Card
+          key={course.id}
+          id={course.id}
+          title={course.title}
+          description={course.description}
+          price={course.price}
+        />
+      ))
+    } else {
+      content = 
+      <Box marginTop={5}>
+        <Typography variant='h4' className='title-poppins'>Nenhum resultado encontrado</Typography>
+      </Box>
+    }
   } else if(status === 'failed') {
     content = <div>{error}</div>
   }
 
 
   return (
-    <Grid
-      container
-      alignItems="center"
-      justifyContent="center"
-      sx={{ marginBottom: '50px' }}
-    >
-      <Grid item container xs={12} sx={{ marginX: 5, justifyContent: 'center' }}>
-        <Typography variant='h2' className='title-poppins'>Cursos</Typography>
-      </Grid>
-      <Grid item xs={12} container justifyContent='center' marginX={5}>
-        {content}
-      </Grid>
+    <Grid title='Cursos'>
+      {content}
     </Grid>
   )
 }
