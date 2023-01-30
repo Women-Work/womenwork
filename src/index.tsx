8d632b875fc1931348aaad16d2b62b5d163d1379
raffaez <rafaeleasouzas@gmail.com>
2023-01-30 00:49:30 -0300

    fix: import theme from mui v5

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,40 @@
-import './index.css';
-import '@fontsource/poppins/600.css';
-import '@fontsource/poppins/700.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "./index.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-import { createTheme, ThemeProvider } from '@material-ui/core';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { persistor, store } from './redux/store';
-import reportWebVitals from './reportWebVitals';
-import Router from './routes/Router';
+import { persistor, store } from "./redux/store";
+import reportWebVitals from "./reportWebVitals";
+import Router from "./routes/Router";
+import { ThemeProvider, createTheme } from "@mui/material";
 
 const theme = createTheme({
   palette: {
     primary: {
-      light: '#7F7498',
-      main: '#766B8D',
-      dark: '#655A7E',
-      contrastText: '#fff'
+      light: "#7F7498",
+      main: "#766B8D",
+      dark: "#534a68",
+      contrastText: "#fff",
     },
     secondary: {
-      light: '#EBBCB7',
-      main: '#E1A6A0',
-      dark: '#C8857F',
-      contrastText: '#fff'
+      light: "#EBBCB7",
+      main: "#E1A6A0",
+      dark: "#C8857F",
+      contrastText: "#fff",
     },
   },
 });
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
