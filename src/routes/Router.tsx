5d53436e1d90673bf40114af7f71ec8c94dd1d45
raffaez <rafaeleasouzas@gmail.com>
2023-01-31 12:33:05 -0300

    rearrange: move routerBrowser to index

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,21 +1,47 @@
-import React from 'react'
-import { useAppSelector } from '../common/hooks'
-import { selectToken } from '../redux/tokenSlice'
-import PrivateRoutes from './PrivateRoutes';
-import PublicRoutes from './PublicRoutes';
-import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import Footer from '../components/static/footer/Footer';
-import Navbar from '../components/static/navbar/Navbar';
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import { useAppDispatch, useAppSelector } from "../common/hooks";
+import Footer from "../components/static/footer/Footer";
+import Navbar from "../components/static/navbar/Navbar";
+import { resetToken, selectToken } from "../redux/tokenSlice";
+import { logout } from "../redux/userSlice";
+import PrivateRoutes from "./PrivateRoutes";
+import PublicRoutes from "./PublicRoutes";
+
+const parseJwt = (token: string) => {
+  try {
+    return JSON.parse(atob(token.split(".")[1]));
+  } catch (e) {
+    return null;
+  }
+};
 
 function Router() {
   const token = useAppSelector(selectToken);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
-  const Routes = () => token && token.length > 0 ? <PrivateRoutes /> : <PublicRoutes />
+  useEffect(() => {
+    if (token) {
+      const decodedJwt = parseJwt(token);
+
+      if (decodedJwt.exp * 1000 < Date.now()) {
+        dispatch(resetToken());
+        dispatch(logout());
+        navigate("/login");
+      }
+    }
+  }, [dispatch, navigate, token]);
+
+  const Routes = () =>
+    token && token.length > 0 ? <PrivateRoutes /> : <PublicRoutes />;
 
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
       <ToastContainer
         position="top-center"
@@ -28,14 +54,13 @@ function Router() {
         pauseOnHover
         theme="light"
       />
-      
-      <div style={{minHeight: 'calc(100vh - 100px)'}} > 
+
+      <div style={{ minHeight: "calc(100vh - 100px)" }}>
         <Routes />
       </div>
       <Footer />
-      {/* <Vlibras forceOnload={true} /> */}
-    </BrowserRouter>
-  )
+    </>
+  );
 }
 
-export default Router
+export default Router;
