d78db01bcf1037e3adabd354840ca117b8649b84
João Vitor Estevam <joaophvt@gmail.com>
2023-01-31 18:45:03 -0300

    feat: display of drawer login and logout buttons

--- src/components/static/drawer/Drawer.tsx
+++ src/components/static/drawer/Drawer.tsx
@@ -1,29 +1,31 @@
 import { VideoLibraryRounded } from "@mui/icons-material";
 import "./Drawer.css";
-
 import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import LoginOutlinedIcon from "@mui/icons-material/LoginOutlined";
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 import MenuIcon from "@mui/icons-material/Menu";
 import MenuBookOutlinedIcon from "@mui/icons-material/MenuBookOutlined";
-import {
-  Box,
-  Divider,
-  Drawer,
-  IconButton,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Typography,
-} from "@mui/material";
+import { Box, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography, } from "@mui/material";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../../common/hooks";
+import { resetToken, selectToken } from "../../../redux/tokenSlice";
+import { logout } from "../../../redux/userSlice";
 
 export default function SDrawer() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const token = useAppSelector(selectToken);
+  let navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  function logoutHandle() {
+    dispatch(resetToken());
+    dispatch(logout());
+    navigate("/login");
+  }
+
   return (
     <>
       <IconButton
@@ -48,12 +50,13 @@ export default function SDrawer() {
         </Box>
 
         <List>
+
           <ListItemButton onClick={() => setIsDrawerOpen(false)}>
             <ListItem component={Link} to="/">
               <ListItemIcon>
                 <HomeOutlinedIcon />
               </ListItemIcon>
-              <ListItemText primary="Home" className="list-item" />
+              <ListItemText primary="Home" className='list-item' />
             </ListItem>
           </ListItemButton>
 
@@ -79,34 +82,57 @@ export default function SDrawer() {
         <Divider />
 
         <List>
-          <ListItemButton onClick={() => setIsDrawerOpen(false)}>
-            <ListItem component={Link} to="/login">
-              <ListItemIcon>
-                <LoginOutlinedIcon />
-              </ListItemIcon>
-              <ListItemText primary="Login" className="list-item" />
-            </ListItem>
-          </ListItemButton>
 
-          <ListItemButton onClick={() => setIsDrawerOpen(false)}>
-            <ListItem component={Link} to="/signup">
-              <ListItemIcon>
-                <AddCircleOutlineOutlinedIcon />
-              </ListItemIcon>
-              <ListItemText primary="Cadastro" className="list-item" />
-            </ListItem>
-          </ListItemButton>
+          {(token == '') ?
 
-          <ListItemButton onClick={() => setIsDrawerOpen(false)}>
-            <ListItem component={Link} to="/user/courses">
-              <ListItemIcon>
-                <VideoLibraryRounded />
-              </ListItemIcon>
-              <ListItemText primary="Meus cursos" className="list-item" />
-            </ListItem>
-          </ListItemButton>
+            <>
+
+              <ListItemButton onClick={() => setIsDrawerOpen(false)}>
+                <ListItem component={Link} to="/signup">
+                  <ListItemIcon>
+                    <AddCircleOutlineOutlinedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Cadastro" className="list-item" />
+                </ListItem>
+
+              </ListItemButton>
+              <ListItemButton onClick={() => setIsDrawerOpen(false)}>
+                <ListItem component={Link} to="/login">
+                  <ListItemIcon>
+                    <LoginOutlinedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Login" className="list-item" />
+                </ListItem>
+              </ListItemButton>
+            </>
+
+            :
+
+            <>
+              <ListItemButton onClick={() => setIsDrawerOpen(false)}>
+                <ListItem component={Link} to="/user/courses">
+                  <ListItemIcon>
+                    <VideoLibraryRounded />
+                  </ListItemIcon>
+                  <ListItemText primary="Meus cursos" className="list-item" />
+                </ListItem>
+              </ListItemButton>
+
+              <ListItemButton onClick={() => setIsDrawerOpen(false)}>
+                <ListItem onClick={() => { logoutHandle(); }}>
+                  <ListItemIcon>
+                    <LogoutOutlinedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Logout" className="list-item" />
+                </ListItem>
+              </ListItemButton>
+
+            </>
+          }
         </List>
       </Drawer>
     </>
   );
 }
+
+
